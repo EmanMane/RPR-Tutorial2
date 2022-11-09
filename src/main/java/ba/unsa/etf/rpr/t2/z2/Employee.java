821ac0aa3e7e806380038726107504528944da9d
Eman Alibalić <ealibalic1@etf.unsa.ba>
2022-11-09 19:41:05 +0100

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/t2/z2/Employee.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.t2.z2;
+
+/**
+ * POJO class for worker
+ * @author Eman Alibalić
+ */
+public class Employee extends Person {
+
+    public Employee(String name, String lastName) {
+        super(name, lastName);
+    }
+}
